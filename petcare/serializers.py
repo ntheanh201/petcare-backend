5291bcbb958cdf08423e99e1c3db17a593676e33
The Anh <ntheanh201@gmail.com>
2020-07-24 16:05:48 +0700

    fix get user info

--- petcare/serializers.py
+++ petcare/serializers.py
@@ -2,16 +2,17 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import PetcareUser
 
+
 class PetcareMemberSerializer(serializers.ModelSerializer):
     # id = serializers.CharField(source="assigned_user.id", read_only=True)
     # username = serializers.CharField(source="assigned_user.username", read_only=True)
-    participating_group = serializers.SerializerMethodField()
+    # participating_group = serializers.SerializerMethodField()
 
-    def get_participating_group(self, obj):
+    # def get_participating_group(self, obj):
         # result_set = GroupPro.objects.filter(members=obj).values("id", "cover", "name")
         # # result_set = GroupPro.objects.filter(members=obj)
         # group_admin_set = GroupPro.objects.filter(admins=obj)
-        response_list = list()
+        # response_list = list()
         # for result in result_set:
         #     response = dict(result)
         #     response["is_admin"] = False
@@ -21,27 +22,23 @@ class PetcareMemberSerializer(serializers.ModelSerializer):
         #     response["cover"] = "http://103.130.218.26:6960/media/" + result["cover"]
         #     response_list.append(response)
         # # return id, "http://103.130.218.26:6960/media/" + cover, name
-        return response_list
+        # return response_list
 
     class Meta:
         model = PetcareUser
-        fields = [
-            "avatar",
-            "url",
-            "id",
-            "username",
-            "display_name",
-            "phone_number",
-            "facebook",
-            "role",
-            "date_of_birth",
-            "description",
-            "email",
-            "participating_group",
-            "user_gender",
-            "cover",
-            "class_name",
-        ]
+        # fields = [
+        #     "avatar",
+        #     "url",
+        #     "id",
+        #     "username",
+        #     "gender",
+        #     "fullname",
+        #     "phone",
+        #     "address",
+        #     "email",
+        #     "dateOfBirth"
+        # ]
+        fields = '__all__'
 
-    def create(self, validated_data):
-        validated_data["validate_data"] = False
+    # def create(self, validated_data):
+    #     validated_data["validate_data"] = False
